bb722070383ef99c3b0ebc4f1f18099a0234f1f4
Emma Hartnall <emmahartnall@gmail.com>
2024-06-25 00:09:53 +1000

    smooth animation

--- animateCircles.js
+++ animateCircles.js
@@ -6,6 +6,8 @@ const animateCircles = (e, x, y) => {
       console.log("circle moved to the left");
       circle.style.left = '100px';
     });
+    //mainImg.style.left = '100px';
+    //maincircle.style.left = '100px';
   }
   else if (x > mX) {
     console.log("mouse moved to the right");
@@ -13,18 +15,26 @@ const animateCircles = (e, x, y) => {
       circle.style.left = '-100px';
       console.log("circle moved to the right");
     });
+    //mainImg.style.left = '-100px';
+    //maincircle.style.left = '-100px';
   }
   if (y < mY) {
     // console.log("moved up");
+    circles.forEach((circle) => {
+      console.log("circle moved up");
+      circle.style.top = '100px';
+    });
+    //mainImg.style.top = '100px';
+    //maincircle.style.top = '100px';
   }
   else if (y > mY) {
     // console.log("moved down");
-  }
-  else if (x == mX) {
-    // console.log("no movement");
-  }
-  else if (y == mY) {
-    // console.log("no movement");
+    circles.forEach((circle) => {
+      console.log("circle moved down");
+      circle.style.top = '-100px';
+    });
+    //mainImg.style.top = '-100px';
+    //maincircle.style.top = '-100px';
   }
 
   mX = e.clientX;
