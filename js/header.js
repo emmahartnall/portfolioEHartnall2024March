2ed3818a0a50f9d57adee1ce7690562e8698481d
Emma Hartnall | Hall <emmahartnall@gmail.com>
2024-07-20 21:30:53 +1000

    removecode

--- js/header.js
+++ js/header.js
@@ -6,7 +6,6 @@ class MyHeader extends HTMLElement {
           <h1>Hartnall Digital Solutions</h1>
           <nav>
             <ul>
-                <li><a href="index.html">Home</a></li>
                 <li><a href="about.html">Emma Hartnall | Hall</a></li>
                 <li><a href="websites.html">Websites</a></li>
                 <li><a href="databases.html">DataBases</a></li>
