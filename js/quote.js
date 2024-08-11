640b48b8bd4f8851a4625941dda185fe7f90c05c
Emma Hartnall | Hall <emmahartnall@gmail.com>
2024-08-11 13:10:11 +1000

    added field data cleaning

--- js/quote.js
+++ js/quote.js
@@ -1,18 +1,21 @@
 document.getElementById('idServiceType').addEventListener('change', function() {
-    var webFields = document.getElementById('webFields');
     
     switch (this.value) {
         case 'web':
-            webFields.classList.remove('hiddenElement');
+            webSiteCreationFields.classList.remove('hiddenElement');
+            dataCleaningFields.classList.add('hiddenElement');
             break;
         case 'data':
-            webFields.classList.add('hiddenElement');
+            dataCleaningFields.classList.remove('hiddenElement');
+            webSiteCreationFields.classList.add('hiddenElement');
             break;
         case 'doc':
-            webFields.classList.add('hiddenElement');
+            webSiteCreationFields.classList.add('hiddenElement');
+            dataCleaningFields.classList.add('hiddenElement');
             break;
         case 'test':
-            webFields.classList.add('hiddenElement');
+            webSiteCreationFields.classList.add('hiddenElement');
+            dataCleaningFields.classList.add('hiddenElement');
             break;
     }
 });
