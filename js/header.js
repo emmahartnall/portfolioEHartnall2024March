bc189f88d811fdfa85abaeb94f092e0a63e5a67b
Emma Hartnall | Hall <emmahartnall@gmail.com>
2024-08-11 20:29:51 +1000

    get rid of services tab

--- js/header.js
+++ js/header.js
@@ -10,12 +10,12 @@ class MyHeader extends HTMLElement {
                 <li><a href="index.html">Home</a></li>
                 <!--<li><a href="about.html">About</a></li>-->
                 <li><a href="people.html">People</a></li>
-                <li><a href="services.html">Services</a></li>
+                <li><a href="quote.html">Price Estimate</a></li>
+                <!--<li><a href="services.html">Services</a></li>-->
                 <li><a href="websites.html">Projects</a></li>
                 <!--<li><a href="databases.html">DataBases</a></li>
                 <li><a href="dataAnalysis.html">Data Analysis</a></li>
                 <li><a href="dataModels.html">Data Models</a></li>--->
-                <li><a href="quote.html">Price Estimate</a></li>
                 <li><a href="contact.html">Contact</a></li>
                 <!--<li><a href="testimonials.html">Testimonials</a></li>--->
             </ul>
