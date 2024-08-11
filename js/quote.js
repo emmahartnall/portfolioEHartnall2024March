7b6df267d5447f74ba9b7b7bee285143f06a1f80
Emma Hartnall | Hall <emmahartnall@gmail.com>
2024-08-12 08:38:57 +1000

    working JavaScript other functions

--- js/quote.js
+++ js/quote.js
@@ -1,89 +1,61 @@
 document.getElementById('idServiceType').addEventListener('change', function() {
     document.getElementById('result').innerHTML = ""
+
+   hideAllFieids();
     
     switch (this.value) {
-        case 'test':
-            testFields.classList.remove('hiddenElement');
-            businessFields.classList.add('hiddenElement');
-            dataFields.classList.add('hiddenElement');
-            databaseFields.classList.add('hiddenElement');
-            modelFields.classList.add('hiddenElement');
-            sysAnaFields.classList.add('hiddenElement');
-            docFields.classList.add('hiddenElement');
-            webSiteCreationFields.classList.add('hiddenElement');
+        case 'data_cleansing':
+            dataCleansingFields.classList.remove('hiddenElement');
             break;
-        case 'business':
-            testFields.classList.add('hiddenElement');
-            businessFields.classList.remove('hiddenElement');
-            dataFields.classList.add('hiddenElement');
-            databaseFields.classList.add('hiddenElement');
-            modelFields.classList.add('hiddenElement');
-            sysAnaFields.classList.add('hiddenElement');
-            docFields.classList.add('hiddenElement');
-            webSiteCreationFields.classList.add('hiddenElement');
+        case 'data_analysis':
+            dataAnalysisFields.classList.remove('hiddenElement');
             break;
-        case 'data':
-            testFields.classList.add('hiddenElement');
-            businessFields.classList.add('hiddenElement');
-            dataFields.classList.remove('hiddenElement');
-            databaseFields.classList.add('hiddenElement');
-            modelFields.classList.add('hiddenElement');
-            sysAnaFields.classList.add('hiddenElement');
-            docFields.classList.add('hiddenElement');
-            webSiteCreationFields.classList.add('hiddenElement');
+        case 'data_models':
+            dataModelFields.classList.remove('hiddenElement');
             break;
         case 'database':
-            testFields.classList.add('hiddenElement');
-            businessFields.classList.add('hiddenElement');
-            dataFields.classList.add('hiddenElement');
             databaseFields.classList.remove('hiddenElement');
-            modelFields.classList.add('hiddenElement');
-            sysAnaFields.classList.add('hiddenElement');
-            docFields.classList.add('hiddenElement');
-            webSiteCreationFields.classList.add('hiddenElement');
             break;
         case 'model':
-            testFields.classList.add('hiddenElement');
-            businessFields.classList.add('hiddenElement');
-            dataFields.classList.add('hiddenElement');
-            databaseFields.classList.add('hiddenElement');
             modelFields.classList.remove('hiddenElement');
-            sysAnaFields.classList.add('hiddenElement');
-            docFields.classList.add('hiddenElement');
-            webSiteCreationFields.classList.add('hiddenElement');
             break;
         case 'sysAna':
-            testFields.classList.add('hiddenElement');
-            businessFields.classList.add('hiddenElement');
-            dataFields.classList.add('hiddenElement');
-            databaseFields.classList.add('hiddenElement');
-            modelFields.classList.add('hiddenElement');
             sysAnaFields.classList.remove('hiddenElement');
-            docFields.classList.add('hiddenElement');
-            webSiteCreationFields.classList.add('hiddenElement');
             break;
         case 'doc':
-            testFields.classList.add('hiddenElement');
-            businessFields.classList.add('hiddenElement');
-            dataFields.classList.add('hiddenElement');
-            databaseFields.classList.add('hiddenElement');
-            modelFields.classList.add('hiddenElement');
-            sysAnaFields.classList.add('hiddenElement');
             docFields.classList.remove('hiddenElement');
-            webSiteCreationFields.classList.add('hiddenElement');
+            break;
+        case 'test_auto':
+            testAutoFields.classList.remove('hiddenElement');
+            break;
+        case 'test_Reg':
+            testRegFields.classList.remove('hiddenElement');
+            break;
+        case 'test-manual':
+            testManualFields.classList.remove('hiddenElement');
             break;
         case 'web':
-            testFields.classList.add('hiddenElement');
-            businessFields.classList.add('hiddenElement');
-            dataFields.classList.add('hiddenElement');
-            databaseFields.classList.add('hiddenElement');
-            modelFields.classList.add('hiddenElement');
-            sysAnaFields.classList.add('hiddenElement');
-            docFields.classList.add('hiddenElement');
             webSiteCreationFields.classList.remove('hiddenElement');
             break;
     }
 });
+
+function hideAllFieids(){
+
+    businessFields.classList.add('hiddenElement');
+    dataCleansingFields.classList.add('hiddenElement');
+    dataAnalysisFields.classList.add('hiddenElement');
+    dataModelFields.classList.add('hiddenElement');
+    databaseFields.classList.add('hiddenElement');
+    modelFields.classList.add('hiddenElement');
+    sysAnaFields.classList.add('hiddenElement');
+    docFields.classList.add('hiddenElement');
+    testAutoFields.classList.add('hiddenElement');
+    testRegFields.classList.add('hiddenElement');
+    testManualFields.classList.add('hiddenElement');
+    webSiteCreationFields.classList.add('hiddenElement');
+  
+}
 
 function calcPriceEstimate(){
     var serviceName = document.getElementById('idServiceType').value;
