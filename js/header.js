4c3b6a39b86f3ab75120e16dd29b351c91e9cb8b
Emma Hartnall | Hall <emmahartnall@gmail.com>
2024-08-12 10:14:13 +1000

    update footer to include linkedin

--- js/header.js
+++ js/header.js
@@ -8,16 +8,10 @@ class MyHeader extends HTMLElement {
           <nav>
             <ul>
                 <li><a href="index.html">Home</a></li>
-                <!--<li><a href="about.html">About</a></li>-->
                 <li><a href="people.html">People</a></li>
                 <li><a href="quote.html">Price Estimate</a></li>
-                <!--<li><a href="services.html">Services</a></li>-->
                 <li><a href="websites.html">Projects</a></li>
-                <!--<li><a href="databases.html">DataBases</a></li>
-                <li><a href="dataAnalysis.html">Data Analysis</a></li>
-                <li><a href="dataModels.html">Data Models</a></li>--->
                 <li><a href="contact.html">Contact</a></li>
-                <!--<li><a href="testimonials.html">Testimonials</a></li>--->
             </ul>
         </nav>
         </header>
@@ -26,4 +20,18 @@ class MyHeader extends HTMLElement {
   }
   
   customElements.define('my-header', MyHeader);
+
+  class MyFooter extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+        <footer>
+        <p>&copy; 2024 Hartnall Digital Solutions. All rights reserved. 
+        <a href="https://www.linkedin.com/company/hartnall-digital-solutions/">
+            <img src="images/LI-Logo.png" alt="LinkedIn" style="height: 1em; vertical-align: middle;"></a>
+        </footer>
+      `;
+    }
+  }
+  
+  customElements.define('my-footer', MyFooter);
   
